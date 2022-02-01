31abd8ff424ae66863bd0bb7fb6e5c2cc12a7472
harry.morgan13@outook.com <harry.morgan13@outook.com>
2022-02-01 14:34:28 +0000

    harry's contribution

--- test.py
+++ test.py
@@ -3,3 +3,5 @@ names = ["Alicia", "Ben", "Charlie", "Harry", "James", "Killian"]
 # This is a test to see if everyone has access to this repository. Can everyone please print their name from the names list and commit the change to Github.
 
 print(names[4])
+
+print(names[3])
