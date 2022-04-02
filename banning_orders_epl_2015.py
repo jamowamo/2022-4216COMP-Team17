f20551bb26e55adfde164cc1163bda5d673189ca
“Benjam-2003” <ben@rjs21.com>
2022-04-02 17:59:36 +0100

    modified epl graph code to rotate tick labels

--- banning_orders_epl_2015.py
+++ banning_orders_epl_2015.py
@@ -1,9 +1,9 @@
 import matplotlib.pyplot as plt
-
+import matplotlib.font_manager as mfont
 team_name = []
 banning_orders = []
 
-f = open('banning_orders.txt', 'r')
+f = open('banning_orders_epl.txt', 'r')
 for row in f:
     row = row.split(',') #Comma indicates the next row on a line
     team_name.append(row[0]) #Reads in the text on a line before the comma
@@ -12,10 +12,10 @@ for row in f:
 fig, ax = plt.subplots() #Creates the figure 
 fig.suptitle("Premier League", fontsize=18) #Sets the suptitle
 ax.set_title("Banning orders in 2015", fontsize=14) #Sets the title
-ax.set_xlabel("Banning Orders", fontsize=12) #sets the x label to year
-ax.set_ylabel("Team Name", fontsize=12) # ^
+ax.set_xlabel("Team name", fontsize=8) #sets the x label to year
+ax.set_ylabel("Number of banning orders", fontsize=12) # ^
 ax.plot(team_name, banning_orders, 'mD:', label='squares') #plots year values along the x axis and total_fans across the y axis. adds squares as the legend for the plotted line
-
+plt.xticks(rotation = 270)
 # ax.set_xlim(1999,2003) #Sets the lower and upper limit of x axis
 # ax.set_ylim(0,100) #y axis limit
 
@@ -23,7 +23,7 @@ ax.plot(team_name, banning_orders, 'mD:', label='squares') #plots year values al
 # ax.yaxis.grid() ^
 
 ax.grid(True) #Creates an x and y axis grid
-ax.legend() #Displays the legend
+# ax.legend() #Displays the legend
 
 plt.show() #Outputs the figure
 
