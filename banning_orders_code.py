7a26e6935f4898e8db9cb139f08dcf5fa6dc9d6d
“Benjam-2003” <ben@rjs21.com>
2022-04-04 23:08:14 +0100

    Updated code for naming scheme change

--- banning_orders_code.py
+++ banning_orders_code.py
@@ -31,22 +31,22 @@ with open('banning_orders_table.csv', 'r') as f:
         row = row.split(',') #Comma indicates the next row on a line
         date.append(row[0]) #Reads and writes to the list the text on a line until it hits a comma
         league.append(row[1])
-        if row[1] == 'Premier League':
+        if row[1] == 'Premier League' and row[0] == '08-Sept-15':
             epl_teams.append(row[2])
             epl_banning_orders.append(int(row[3]))
-        if row[1] == 'Championship':
+        if row[1] == 'Championship' and row[0] == '08-Sept-15':
             champ_teams.append(row[2])
             champ_banning_orders.append(int(row[3]))
-        if row[1] == 'League One':
+        if row[1] == 'League One' and row[0] == '08-Sept-15':
             efl_one_teams.append(row[2])
             efl_one_banning_orders.append(int(row[3]))
-        if row[1] == 'League Two':
+        if row[1] == 'League Two' and row[0] == '08-Sept-15':
             efl_two_teams.append(row[2])
             efl_two_banning_orders.append(int(row[3]))
-        if row[1] == 'National League':
+        if row[1] == 'National League' and row[0] == '08-Sept-15':
             national_league_teams.append(row[2])
             national_league_banning_orders.append(int(row[3]))
-        if row[1] == 'Other clubs':
+        if row[1] == 'Other clubs' and row[0] == '08-Sept-15':
             other_clubs.append(row[2])
             other_clubs_banning_orders.append(int(row[3]))
 
@@ -59,12 +59,12 @@ ax.set_ylabel("Number of banning orders", fontsize=12)
 ax.bar(champ_teams,champ_banning_orders)
 plt.xticks(rotation = 270)
 
-ax.set_yticks(range(0,150,10))
+ax.set_yticks(range(0,150,5))
 # ax.set_xlim(0,150) #Sets the lower and upper limit of x axis
 ax.set_ylim(0,150) #y axis limit
 
 # ax.xaxis.grid() can be used if only an x axis grid is desired
-# ax.yaxis.grid() ^
+ax.yaxis.grid()
 
 # ax.grid(True) #Creates an x and y axis grid
 # ax.legend() #Displays the legend
