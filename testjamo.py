78938aa6100fff414a9f5781ccf18d9798b8b5fb
jamowamo <jamesjscott03@gmail.com>
2022-04-05 20:32:45 +0100

    editing arrest comp table

--- testjamo.py
+++
@@ -1,8 +0,0 @@
-import csv
-import matplotlib
-
-with open("arrest rate by competition.csv" , "r") as f:
-    csv_reader = csv.reader(f)
-    header_row = next(csv_reader)
-    print(header_row)
-    
