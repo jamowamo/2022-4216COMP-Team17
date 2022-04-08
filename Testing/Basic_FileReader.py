e62465631b6982b15b630be25adb41185ea8b07c
“Benjam-2003” <ben@rjs21.com>
2022-04-08 19:23:12 +0100

    Deleted Testing folder

--- Testing/Basic_FileReader.py
+++
@@ -1,6 +0,0 @@
-
-with open('sample.txt', 'r')as f:
-    text = f.read()
-
-print(text)
-
--- Testing/Competiton_List.py
+++
@@ -1,1 +0,0 @@
-comps = ['championship', 'european competitions', 'fa cup', 'football league trophy', 'internationals', 'league cup', 'league one', 'league two', 'national league', 'premier league']
